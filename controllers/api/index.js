1b435f9538baa31fea9474f496cc5f886240077e
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-30 14:42:59 -0400

    yup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const userRoutes = require('./userRoute');
+const blogRoutes = require('./blogRoute');
+const commentRoutes = require('./commentRoute')
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/comments', commentRoutes);
+router.use('/blogs', blogRoutes);
 
 module.exports = router;
