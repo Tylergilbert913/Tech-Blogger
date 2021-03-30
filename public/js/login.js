ebeaff977a2755afea539c2cff41e5a269550b81
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-30 14:52:46 -0400

    filled in the login file or public folder

--- public/js/login.js
+++ public/js/login.js
@@ -28,11 +28,12 @@ const loginFormHandler = async (event) => {
     const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
   
-    if (name && email && password) {
+    if (name && email && password && username) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ name, email, password, username }),
         headers: { 'Content-Type': 'application/json' },
       });
   
