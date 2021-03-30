01e199470e7e08055d3ec01d760bc8d1532d9511
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-29 21:57:55 -0400

    created the comment file for the models and adjusted the syntax layour for all model files

--- models/blog.js
+++ models/blog.js
@@ -1,39 +1,39 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Blog extends Model { }
 
 Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        blog_content: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    blog_content: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'blog',
-  }
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+    }
 );
 
 module.exports = Blog;
